722cf1c3a674735fff6ff33a4bb151846b52582f
TJ Struck <>
2021-05-27 00:48:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+string FirstName;
+string LastName;
+string vehicle;
+int year;
+
+int option;
+
+int main()
+{
+  cout << "Welcome to detailbase 1.0 \n";
+  cout << "Please select an option. \n\n";
+
+  cout << "1. Add New Customer \n";
+  cout << "2. Add New Vehicle \n";
+  cout << "3. Look Up Customer \n";
+  cout << "4. Create A Detail Entry\n\n";
+
+  cin >> option;
+
+  if(option == 1)
+  {
+    cout << "First Name: ";
+    cin >> FirstName;
+    cout << "Last Name: ";
+    cin >> LastName;
+    cout << "Vehicle (example: Dodge Charger RT): ";
+    cin >> vehicle;
+    cout << "Year: ";
+    cin >> year;
+    
+    cout << "\n\n Thanks! " << FirstName << " " << LastName << " has been added!";
+  }
+
+}
